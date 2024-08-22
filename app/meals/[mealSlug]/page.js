6360a82a627964484a4306e362c706fd43bad015
Fenil Patel <fenilmayurkumar.patel@student.griffith.ie>
2024-08-22 03:02:02 +0100

    added meal page and updated with db

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
-export default function something({params}) {
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+
+export default function something({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if(!meal){
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <div><h1>{params.params}</h1></div>
-  )
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
