3f067c1130bc8e241eea5ed7b09ac469b5cba1cc
Fenil Patel <fenilmayurkumar.patel@student.griffith.ie>
2024-08-22 02:35:37 +0100

    added error and not found page

---
+++ app/meals/error.js
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function error() {
+  return (
+    <main className="error">
+      <h1>An error ocurred!</h1>
+      <p>Failed to load meal data. Please tray again later.</p>
+    </main>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Notfound() {
+  return (
+    <main className="not-found">
+      <h1>Page Not Found</h1>
+      <p>Unfortunately, we could not find the requested page or resources.</p>
+    </main>
+  );
+}
