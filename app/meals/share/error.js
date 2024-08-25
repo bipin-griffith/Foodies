84d9fcd64bee57023b1aeff7e5a2ffe9852345f3
Fenil Patel <fenilmayurkumar.patel@student.griffith.ie>
2024-08-25 07:04:51 +0100

    added validation to form

---
+++ app/meals/share/error.js
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function error() {
+  return (
+    <main className="error">
+      <h1>An error ocurred!</h1>
+      <p>Invalid Input. Please Try Agian</p>
+    </main>
+  );
+}
